ece9a153ab7b5457d836c26a5ffde41a15d47211
woals4815 <71315300+woals4815@users.noreply.github.com>
2020-11-06 17:20:28 +0900

    install PartialType & make update-movie.dto

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -28,7 +29,7 @@ export class MoviesController {
   }
 
   @Patch(':id')
-  patch(@Param('id') movieId: number, @Body() updateData) {
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
     return this.moviesService.update(movieId, updateData);
   }
 }
