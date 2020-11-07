64de087110f7cdea66983125786a15050a5f3156
woals4815 <71315300+woals4815@users.noreply.github.com>
2020-11-07 17:17:03 +0900

    Finished making API

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -30,12 +30,20 @@ describe('AppController (e2e)', () => {
     it('GET', ()=>{
       return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
     });
-    it('POST', ()=>{
+    it('POST 201', ()=>{
       return request(app.getHttpServer()).post('/movies').send({
         title: "Test",
         year: 2000,
         genres: ['test']
-      });
+      }).expect(201);
+    });
+    it('POST 400', ()=>{
+      return request(app.getHttpServer()).post('/movies').send({
+        title: "Test",
+        year: 2000,
+        genres: ['test'],
+        other: "thing",
+      }).expect(400);
     });
     it('DELETE', ()=> {
       return request(app.getHttpServer()).delete('/movies').expect(404);
@@ -48,7 +56,11 @@ describe('AppController (e2e)', () => {
     it('GET 404', ()=>{
       return request(app.getHttpServer()).get('/movies/999').expect(404);
     });
-    it.todo('DELETE');
-    it.todo('PATCH');
+    it('PATCH 200', ()=>{
+      return request(app.getHttpServer()).patch('/movies/1').send({ title: "Updated Test"} ).expect(200);
+    });
+    it('DELETE 200', ()=>{
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });    
   });
 });
